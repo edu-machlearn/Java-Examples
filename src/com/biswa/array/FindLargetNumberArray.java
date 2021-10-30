94de946cc4f26d04da8c342ea61aa307c7295566
Biswaranjan Mohapatra <info.machlearn@gmail.com>
2021-10-30 11:53:20 +0530

    FindLargestNumberArray

--- src/com/biswa/array/FindLargetNumberArray.java
+++ src/com/biswa/array/FindLargetNumberArray.java
@@ -1,7 +1,26 @@
 package com.biswa.array;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class FindLargetNumberArray {
     public static void main(String[] args) {
-        System.out.println("Git Examples..");
+        Integer arr[]={10,20,40,23,300,200,98};
+        Arrays.sort(arr);
+        System.out.println(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        System.out.println("Second Largest Number is "+(arr[arr.length-2]));
+
+        List<Integer> intList= (List<Integer>) Arrays.asList(arr);
+        List<Integer> nulList = intList.stream()
+                .filter(numberGt100 -> numberGt100 >= 100)
+                .collect(Collectors.toList());
+        //.count();
+
+        System.out.println("Count -"+nulList.stream().count());
+        nulList.forEach(printNumber-> System.out.println(printNumber));
     }
 }
